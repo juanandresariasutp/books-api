acaec82dcdaa6ed41b209643b67fc174fd2a200e
JuanAndres2002 <juanandres.arias@utp.edu.co>
2026-03-12 10:38:54 -0500

    Fix: changes in config

--- db/config.js
+++ db/config.js
@@ -1,7 +1,15 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const poolConfig = {
   connectionString: process.env.DATABASE_URL,
-});
+};
+
+if (process.env.NODE_ENV === 'production') {
+  poolConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
